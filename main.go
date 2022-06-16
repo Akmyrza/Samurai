364e49810c251c940c9b9fc91c5d54d45ec18e3b
Akmyrza <onlyfordox@mail.ru>
2022-06-16 10:47:12 +0600

    changed

--- main.go
+++ main.go
@@ -1,14 +1,17 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
+	"log"
 	"os"
 	"sort"
+	"time"
 )
 
 type Word struct {
-	Name string
+	Name  []byte
 	Count int
 }
 
@@ -18,44 +21,63 @@ func (a ByCount) Len() int           { return len(a) }
 func (a ByCount) Less(i, j int) bool { return a[i].Count > a[j].Count }
 func (a ByCount) Swap(i, j int)      { a[i], a[j] = a[j], a[i] }
 
+func CompareTwoByteSlices(a, b []byte) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i, v := range a {
+		if v != b[i] {
+			return false
+		}
+	}
+	return true
+}
+
 func main() {
 
-	f, _ := os.Open("mobydick.txt")
-	res, _ := ioutil.ReadAll(f)	
+	start := time.Now()
 
-	var str string
-	var Words[] Word
-	var found bool = false
+	var path = flag.String("path", "default value", "path to file")
+	flag.Parse()
+
+	f, err := os.Open(*path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer f.Close()
+
+	res, _ := ioutil.ReadAll(f)
+
+	var str []byte
+	var Words []Word
+	var found bool
 
 	for _, elem := range res {
 
-		if (65 <= elem && elem <= 90) {
-			str = str + string(elem + 32)			
-		}
-
-		if (97 <= elem && elem <= 122) {
-			str = str + string(elem)			
-		}
-		
-		if elem == 32 || elem == 10 {
-			if str != "" {				
+		if 65 <= elem && elem <= 90 {
+			str = append(str, elem+32)
+		} else if 97 <= elem && elem <= 122 {
+			str = append(str, elem)
+		} else {
+			if str != nil {
 
 				found = false
 
 				for i, v := range Words {
-					if v.Name == str {
-						Words[i].Count++ 
+					if CompareTwoByteSlices(v.Name, str) {
+						Words[i].Count++
 						found = true
 						break
 					}
 				}
 
 				if !found {
-					Words = append(Words, Word{str, 1})				
+					Words = append(Words, Word{str, 1})
 				}
 
-				str = ""
+				str = nil
 			}
+
 		}
 
 	}
@@ -63,11 +85,13 @@ func main() {
 	sort.Sort(ByCount(Words))
 
 	for i, elem := range Words {
-		fmt.Println(elem)
+		stringfrombytes := fmt.Sprintf("%s", elem.Name)
+		fmt.Println(stringfrombytes, elem.Count)
 		if i == 20 {
 			break
 		}
 	}
 
-	f.Close()
+	elapsed := time.Since(start)
+	fmt.Printf("time %s", elapsed)
 }
